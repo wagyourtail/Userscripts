99c1bab8d59c133bac55abc71ab9744887d9208d
William Gray <ruwagray@gmail.com>
2025-10-29 05:47:15 -0500

    Update version and improve wiki link creation

--- wanderinginn.user.js
+++ wanderinginn.user.js
@@ -2,12 +2,12 @@
 
 // @name         Wandering Inn Chapters
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Add wiki urls to wandering inn chapter list
 // @author       Wagyourtail
 // @downloadURL  https://github.com/wagyourtail/Userscripts/raw/refs/heads/main/wanderinginn.user.js
 // @updateURL    https://github.com/wagyourtail/Userscripts/raw/refs/heads/main/wanderinginn.user.js
-// @match        https://wanderinginn.com/table-of-contents/*
+// @match        https://wanderinginn.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=wanderinginn.com
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -15,19 +15,28 @@
 (function() {
     'use strict';
 
-     [...document.getElementsByClassName("body-web")].forEach(e => {
-         const c = document.createElement("a")
-         const sup = document.createElement("sup")
-         if (/^\d.+/.test(e.innerText)) {
-             c.href = `https://wiki.wanderinginn.com/Chapter_${e.innerText.replaceAll(" ", "_")}`
-         } else {
-             c.href = `https://wiki.wanderinginn.com/${e.innerText.replaceAll(" ", "_")}`
-         }
-         c.appendChild(sup)
-         c.classList.add("wiki_ref")
-         sup.appendChild(document.createTextNode("(wiki)"))
-         e.appendChild(c)
-     })
+    function createWikiElement(e) {
+        const c = document.createElement("a")
+        const sup = document.createElement("sup")
+        if (/^\d.+/.test(e.innerText)) {
+            c.href = `https://wiki.wanderinginn.com/Chapter_${e.innerText.replaceAll(" ", "_")}`
+        } else {
+            c.href = `https://wiki.wanderinginn.com/${e.innerText.replaceAll(" ", "_")}`
+        }
+        c.appendChild(sup)
+        c.classList.add("wiki_ref")
+        sup.appendChild(document.createTextNode("(wiki)"))
+        return c
+    }
+
+    if (window.location.pathname.startsWith("table-of-contents")) {
+        [...document.getElementsByClassName("body-web")].forEach(e => {
+            e.appendChild(createWikiElement(e))
+        })
+    } else {
+        const e = document.getElementsByClassName("entry-title")[0]
+        e.appendChild(createWikiElement(e))
+    }
 
      GM_addStyle(`
      .wiki_ref {
